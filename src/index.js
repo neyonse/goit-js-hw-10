05b7aa04fb9e8d4805630dab6fd094f053431e86
neyonse <neyonse@gmail.com>
2023-06-12 16:16:46 +0300

    update homework

--- src/index.js
+++ src/index.js
@@ -1,35 +1,46 @@
-import { fetchBreeds } from './js/cat-api';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
 const breedSelectorEl = document.querySelector('.breed-select');
 const catInfoEl = document.querySelector('.cat-info');
 
-fetchBreeds().then(breeds => {
-  console.log(breeds);
-  fillBreedSelectorEl(breeds);
-});
+fetchBreeds().then(breeds => fillBreedSelectorEl(breeds));
 
 function fillBreedSelectorEl(breeds) {
   breeds.forEach(breed => {
     const optionEl = document.createElement('option');
 
     optionEl.textContent = breed.name;
+    optionEl.value = breed.id;
     breedSelectorEl.appendChild(optionEl);
   });
 }
 
-breedSelectorEl.addEventListener('change', event => {
-  const selectedBreedName = event.currentTarget.value;
-  updateDescriptionName(selectedBreedName);
+breedSelectorEl.addEventListener('change', e => {
+  const selectedOption = e.target.options[e.target.selectedIndex];
+
+  fetchCatByBreed(selectedOption.value)
+    .then(selectedCat => gatherSelectedCatData(selectedCat))
+    .then(selectedCatData => updateCatInfo(selectedCatData));
 });
 
-function updateDescriptionName(selectedBreedName) {
-  let descriptionNameEl = catInfoEl.querySelector('h2');
+function gatherSelectedCatData(fetchedCat) {
+  return (selectedCatData = {
+    breed: fetchedCat[0].breeds[0].name,
+    img: fetchedCat[0].url,
+    imgW: fetchedCat[0].width,
+    imgH: fetchedCat[0].height,
+    descr: fetchedCat[0].breeds[0].description,
+    temperament: fetchedCat[0].breeds[0].temperament,
+  });
+}
 
-  if (!descriptionNameEl) {
-    descriptionNameEl = document.createElement('h2');
-    catInfoEl.appendChild(descriptionNameEl);
-  }
-
-  descriptionNameEl.textContent = selectedBreedName;
-  descriptionNameEl.classList.add('cat-name');
+function updateCatInfo({ breed, img, imgW, imgH, descr, temperament }) {
+  catInfoEl.innerHTML = `
+  <img src=${img} width=${imgW} height=${imgH}>
+  <div class='cat-info-wrap'>
+  <h2>${breed}</h2>
+  <p><span>Description:</span> ${descr}</p>
+  <p><span>Temperament:</span> ${temperament}</p>
+  </div>
+  `;
 }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,6 +1,30 @@
 const MY_API_KEY =
   'live_puUKGgmlnUJcfdimVBdpD4oNPThpUOR4QA0rTiTbaq8It42r4X3vg0EFqQtB0Dze';
 
+export function fetchBreeds() {
+  const url = 'https://api.thecatapi.com/v1/breeds';
+
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error('Ми не змогли знайти де лежать котики', response.status);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  const url = 'https://api.thecatapi.com/v1/images/search';
+
+  return fetch(`${url}?breed_ids=${breedId}&api_key=${MY_API_KEY}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error('', response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
 // fetch(url)
 //   .then(function (response) {
 //     return response.json(); //отримуємо response(відповідь) у форматі json від сервера, і кажемо, що нам потрібно перетворити її у джава об'єкт за допомогою методу json().
@@ -13,33 +37,6 @@ const MY_API_KEY =
 //   });
 
 // fetch(url)
-//   .then(response => response.json()) //отримуємо response(відповідь) у форматі json від сервера, і кажемо, що нам потрібно перетворити її у джава об'єкт за допомогою методу json().
-//   .then(data => console.log(data)) //тут ми вже маємо отримані дані у вигляді з яким можемо далі працювати та прописуємо, що із ними робити далі.)
-//   .catch(error => console.log('Сталася помилка:', error));
-
-export function fetchBreeds() {
-  const url = 'https://api.thecatapi.com/v1/breeds';
-
-  // return fetch(url)
-  //   .then(response => response.json())
-  //   .catch(error => console.log(error));
-
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error('Ми не змогли знайти де лежать котики', response.status);
-    }
-    return response.json();
-  });
-}
-
-// export function fetchCatByBreed(breedId) {
-//   const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${MY_API_KEY}`;
-
-//   return fetch(url)
-//     .then(response => response.json())
-//     .then(data => data)
-//     .catch(error => {
-//       console.error('Error fetching cat by breed:', error);
-//       throw error;
-//     });
-// }
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
